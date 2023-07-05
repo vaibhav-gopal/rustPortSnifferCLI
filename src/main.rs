93fe24ff9e4747dc7abd32f4f330d089e10d9d5d
Vaibhav Gopal <vabsgop@gmail.com>
2023-07-05 16:51:14 -0400

    Finished portsniffer CLI tool

---
+++ src/main.rs
@@ -0,0 +1,114 @@
+use std::{
+    env,
+    io::{self, Write},
+    net::{IpAddr, TcpStream},
+    process,
+    str::FromStr,
+    sync::mpsc::{channel, Sender},
+    thread,
+};
+
+const MAX: u16 = 65535;
+
+//CLI tool will accept 1 flag which will tell how many threads to execute
+//  and will accept one ip address to port sniff
+//usage --> `cargo run -- -j 20 127.0.0.1` to sniff for ports with 20 threads using current computer's ip address
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        } else if args.len() > 4 {
+            return Err("Too many arguments");
+        }
+        let f = args[1].clone();
+        if let Ok(ipaddr) = IpAddr::from_str(&f) {
+            return Ok(Arguments {
+                flag: String::from(""),
+                ipaddr,
+                threads: 4,
+            });
+        } else {
+            if f.contains("-h") || f.contains("--help") && args.len() == 2 {
+                println!("Usage: -j to select how many threads you want\r\n\t-h or -help to show this help message");
+                return Err("help");
+            } else if f.contains("-h") || f.contains("--help") {
+                return Err("Too many arguments");
+            } else if f.contains("-j") {
+                let ipaddr = match IpAddr::from_str(&args[3]) {
+                    Ok(s) => s,
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
+                };
+                let threads = match args[2].parse::<u16>() {
+                    Ok(s) => s,
+                    Err(_) => return Err("Failed to parse thread number"),
+                };
+                return Ok(Arguments {
+                    flag: f,
+                    ipaddr,
+                    threads,
+                });
+            } else {
+                return Err("invalid syntax");
+            }
+        }
+    }
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => (),
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
+        if err.contains("help") {
+            process::exit(0);
+        } else {
+            eprintln!("{} problem parsing arguments: {}", args[0], err);
+            process::exit(0);
+        }
+    });
+
+    let num_threads = arguments.threads;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, arguments.ipaddr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
+}
