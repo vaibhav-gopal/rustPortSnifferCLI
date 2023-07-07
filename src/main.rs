84216328afaa4bee4eefd6fcad28dc2139d6c69e
Vaibhav Gopal <vabsgop@gmail.com>
2023-07-06 20:44:03 -0400

    updated --> efficiency and clarity

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,63 @@
 use std::{
-    env,
     io::{self, Write},
-    net::{IpAddr, TcpStream},
-    process,
-    str::FromStr,
+    net::{IpAddr, Ipv4Addr},
     sync::mpsc::{channel, Sender},
-    thread,
 };
+use bpaf::Bpaf;
+use tokio::{
+    task,
+    net::TcpStream,
+};
+// Tokio is a runtime library that implements a task / call queue to run mutliple computations quickly and concurrently --> don't know whether it uses async/await or threads to acheive this or both
+// Bpaf is a terminal tool, that lets us add menus, colors etc. --> the macro attribute lets us define menu options right in the implementation
 
 const MAX: u16 = 65535;
 
-//CLI tool will accept 1 flag which will tell how many threads to execute
-//  and will accept one ip address to port sniff
-//usage --> `cargo run -- -j 20 127.0.0.1` to sniff for ports with 20 threads using current computer's ip address
+const IPFALLBACK: IpAddr = IpAddr::V4(Ipv4Addr::new(127,0,0,1));
 
-struct Arguments {
-    flag: String,
-    ipaddr: IpAddr,
-    threads: u16,
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options)]
+pub struct Arguments {
+    #[bpaf(long, short, fallback(IPFALLBACK))]
+    /// The address that you want to port sniff. Must be a valid IPV4 address. Falls back to current device
+    pub address: IpAddr,
+    #[bpaf(long("start"), short('s'), fallback(1u16), guard(start_port_guard, "must be greater than 0"))]
+    /// The start port for the sniffer. (must be greater than 0)
+    pub start_port: u16,
+    #[bpaf(long("end"), short('e'), fallback(MAX), guard(end_port_guard, "must be less than or equal to 65535"))]
+    /// The end port for the sniffer. (must be less than or equal to 65535)
+    pub end_port: u16,
 }
 
-impl Arguments {
-    fn new(args: &[String]) -> Result<Arguments, &str> {
-        if args.len() < 2 {
-            return Err("Not enough arguments");
-        } else if args.len() > 4 {
-            return Err("Too many arguments");
+// Bpaf uses these functions when taking in inputs to the start/end port field in the Arguments struct --> acts as a guard or input verifier
+fn start_port_guard(input: &u16) -> bool {
+    *input > 0
+}
+fn end_port_guard(input: &u16) -> bool {
+    *input <= MAX
+}
+
+async fn scan(tx: Sender<u16>, port: u16, addr: IpAddr) {
+    match TcpStream::connect(format!("{}:{}",addr, port)).await {
+        Ok(_) => {
+            print!(".");
+            io::stdout().flush().unwrap();
+            tx.send(port).unwrap();
         }
-        let f = args[1].clone();
-        if let Ok(ipaddr) = IpAddr::from_str(&f) {
-            return Ok(Arguments {
-                flag: String::from(""),
-                ipaddr,
-                threads: 4,
-            });
-        } else {
-            if f.contains("-h") || f.contains("--help") && args.len() == 2 {
-                println!("Usage: -j to select how many threads you want\r\n\t-h or -help to show this help message");
-                return Err("help");
-            } else if f.contains("-h") || f.contains("--help") {
-                return Err("Too many arguments");
-            } else if f.contains("-j") {
-                let ipaddr = match IpAddr::from_str(&args[3]) {
-                    Ok(s) => s,
-                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
-                };
-                let threads = match args[2].parse::<u16>() {
-                    Ok(s) => s,
-                    Err(_) => return Err("Failed to parse thread number"),
-                };
-                return Ok(Arguments {
-                    flag: f,
-                    ipaddr,
-                    threads,
-                });
-            } else {
-                return Err("invalid syntax");
-            }
-        }
+        Err(_) => (),
     }
 }
 
-fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
-    let mut port: u16 = start_port + 1;
-    loop {
-        match TcpStream::connect((addr, port)) {
-            Ok(_) => {
-                print!(".");
-                io::stdout().flush().unwrap();
-                tx.send(port).unwrap();
-            }
-            Err(_) => (),
-        }
+#[tokio::main]
+async fn main() {
+    //bpaf creates a function with the same name as the struct to implement the command line menu
+    let opts: Arguments = arguments().run();
 
-        if (MAX - port) <= num_threads {
-            break;
-        }
-        port += num_threads;
-    }
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
-        if err.contains("help") {
-            process::exit(0);
-        } else {
-            eprintln!("{} problem parsing arguments: {}", args[0], err);
-            process::exit(0);
-        }
-    });
-
-    let num_threads = arguments.threads;
     let (tx, rx) = channel();
-    for i in 0..num_threads {
+    for i in opts.start_port..=opts.end_port {
         let tx = tx.clone();
-
-        thread::spawn(move || {
-            scan(tx, i, arguments.ipaddr, num_threads);
+        task::spawn(async move {
+            scan(tx,i, opts.address).await;
         });
     }
 
